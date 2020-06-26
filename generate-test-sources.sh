6d5b2871f3488572b6c9ea46e44c26f8e82209f8
Semih Celik <s.celikce@gmail.com>
2020-06-26 09:56:21 +0300

    Change profile names (#4)

--- generate-test-sources.sh
+++ generate-test-sources.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-mvn -DskipTests -P prepare-for-upload package
+PROFILE=$1
+
+if [[ $PROFILE != "android" && $PROFILE != "ios" ]]; then
+  echo "Profile not found. it should be 'ios' or 'android'"
+  exit 0
+  fi
+
+echo "Generating sources for platform ${PROFILE} started"
+
+mvn clean package -DskipTests -P prepare-for-upload,$PROFILE
+
+echo "Generating sources for platform ${PROFILE} finished"
